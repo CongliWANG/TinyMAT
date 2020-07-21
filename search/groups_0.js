ad8e9e7dfa2be68ee9a697e2a32a8544d2f59dfb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-21 12:28:57 +0000

    Deploy jkriege2/TinyMAT to github.com/jkriege2/TinyMAT.git:gh-pages

---
+++ search/groups_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['tiny_20matlab_28r_29_20mat_20writer_20library_107',['Tiny Matlab(r) MAT writer library',['../group__tinymatwriter.html',1,'']]],
+  ['tinymatwriter_2dsupport_20for_20opencv_20datatypes_108',['TinyMATWriter-Support for OpenCV Datatypes',['../group__tinymatwriter__opencv.html',1,'']]],
+  ['tinymatwriter_2dsupport_20for_20qt_20datatypes_109',['TinyMATWriter-Support for Qt Datatypes',['../group__tinymatwriter__qt.html',1,'']]]
+];
